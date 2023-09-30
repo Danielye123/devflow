f1ee827719b523b3ec3ff7a07b393a3a3e40d362
Daniel <daniel_ye.nz@hotmail.com>
2023-09-29 23:51:29 -0700

    start navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
- 
 export default function Home() {
   return (
     <div>
       Hi there!
-      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
