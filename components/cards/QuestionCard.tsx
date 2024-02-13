c1479526993af278bfa2bde98076c4539e71267e
Daniel <daniel_ye.nz@hotmail.com>
2024-02-12 22:44:44 -0800

    add wrapper to card div

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 
 interface QuestionProps {
+  _id: string;
+  title: string;
+  tags: {
     _id: string;
-    title: string;
-    tags: {
-        _id: string;
-        name: string;
-    }[];
-    author: {
-        _id: string;
-        name: string;
-        picture: string;
-    }
-    upvotes: number;
-    views: number;
-    answers: Array<object>;
-    createdAt: Date;
+    name: string;
+  }[];
+  author: {
+    _id: string;
+    name: string;
+    picture: string;
+  };
+  upvotes: number;
+  views: number;
+  answers: Array<object>;
+  createdAt: Date;
 }
 
-const QuestionCard = ({ _id, title, tags, author, upvotes, views, answers, createdAt }: QuestionProps) => {
-  return <div>QuestionCard</div>;
+const QuestionCard = ({
+  _id,
+  title,
+  tags,
+  author,
+  upvotes,
+  views,
+  answers,
+  createdAt,
+}: QuestionProps) => {
+  return <div className="card-wrapper p-9 sm:px-11">QuestionCard</div>;
 };
 
 export default QuestionCard;
