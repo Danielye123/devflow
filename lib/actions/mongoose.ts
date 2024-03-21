aacb3578bbf1d9117339a1833a3fdf0aa1c6da06
Daniel <daniel_ye.nz@hotmail.com>
2024-03-21 00:43:01 -0700

    connect to mongodb and server actions setup

---
+++ lib/actions/mongoose.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+
+export const connectToDatabase = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(!process.env.MONGODB_URL) return console.log('MISSING MONGODB_URL');
+
+    if (isConnected){
+        console.log('MongoDb is already connected');
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            dbName: 'Devflow'
+        })
+
+        isConnected = true;
+        console.log('MongoDB is connected');
+    } catch (error) {
+        console.log('MongoDB connection failed');
+    }
+}
