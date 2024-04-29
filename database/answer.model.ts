82f166aa24fba0cc7b9e12535f85390b8a7b54ca
Daniel <daniel_ye.nz@hotmail.com>
2024-04-28 22:03:03 -0700

    finish answer model/schema

---
+++ database/answer.model.ts
@@ -0,0 +1,23 @@
+import { Schema, model, models, Document } from "mongoose";
+
+export interface IAnswers extends Document {
+    author: Schema.Types.ObjectId;
+    question: Schema.Types.ObjectId;
+    content: string;
+    upvotes: Schema.Types.ObjectId[];
+    downvotes: Schema.Types.ObjectId[];
+    createdAt: Date;
+}
+
+const AnswersSchema = new Schema<IAnswers>({
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    question: { type: Schema.Types.ObjectId, required: true, ref: 'Question' },
+    content: [{ type: String, required: true }],
+    upvotes: [{ type: Schema.Types.ObjectId, ref: 'User' }], 
+    downvotes: [{ type: Schema.Types.ObjectId, ref: 'User' }], 
+    createdAt: { type: Date, default: Date.now },
+});
+
+const Answer = models.Answer || model("Answer", AnswersSchema);
+
+export default Answer;
