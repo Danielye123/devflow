389bef247018e4ae84e2fb73f4eeb36005768bd9
Daniel <daniel_ye.nz@hotmail.com>
2024-05-04 23:23:43 -0700

    ..

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -27,7 +27,7 @@ export async function createAnswer(params: CreateAnswerParams) {
   }
 }
 
-//comment
+
 export async function getAnswers(params: GetAnswersParams) {
   try {
     connectToDatabase();
