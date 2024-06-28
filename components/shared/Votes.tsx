d2d1f65541f6453d7d6bf72eee547ca1c897d249
Daniel <daniel_ye.nz@hotmail.com>
2024-06-27 22:08:13 -0700

    finish voting functionality

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { downvoteQuestion, upvoteQuestion } from "@/lib/actions/question.action";
 import { formatNumber } from "@/lib/utils";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   type: string;
@@ -25,12 +27,59 @@ const Votes = ({
   hasSaved,
 }: Props) => {
 
+  const pathname = usePathname();
+  const router = useRouter();
+
   const handleSave = () => {
 
   }
 
-  const handleVote = (action: string) => {
+  const handleVote = async (action: string) => {
     if(!userId){
+      return;
+    }
+
+    if(action === 'upvote') {
+      if(type === 'Question') {
+        await upvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        })
+      } else if(type === 'Answer'){
+        // await upvoteAnswer({
+        //   questionId: JSON.parse(itemId),
+        //   userId: JSON.parse(userId),
+        //   hasupVoted,
+        //   hasdownVoted,
+        //   path: pathname,
+        // })
+      }
+      // TODO: show a toast
+      return;
+    }
+
+    if(action === 'downvote') {
+      if(type === 'Question') {
+        await downvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        })
+      } else if(type === 'Answer'){
+        // await downvoteAnswer({
+        //   questionId: JSON.parse(itemId),
+        //   userId: JSON.parse(userId),
+        //   hasupVoted,
+        //   hasdownVoted,
+        //   path: pathname,
+        // })
+      }
+      // TODO: show a toast
       return;
     }
   }
@@ -48,7 +97,7 @@ const Votes = ({
             width={18}
             height={18}
             className="cursor-pointer"
-            // onClick={() => handleVote('upvote')}
+            onClick={() => handleVote('upvote')}
           />
 
           <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
